84d39d270f13d48265f5f10d557aaa1b38c8a2dd
Louis Win <88466438+lwin496@users.noreply.github.com>
2023-01-06 15:14:26 -0700

    pushing boiler plate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <div className="bg-purple-800 h-[100vh] text-white font-bold text-2xl">
+        <h1 className="flex justify-center pt-[10vh]">Password Generator</h1>
+      </div>
+    </>
+   
+  );
+}
+
+export default App;
